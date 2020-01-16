786144f4eee628cb4c3f2ab3c89a4bd64559ddce
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2020-01-16 15:20:29 -0600

    Create hello1.c
    this my first repository which has changes that include my major

---
+++ hello1.c
@@ -0,0 +1,14 @@
+/*
+*Gabin Kundwa
+*January 16 2020
+*CSCE 155
+*Create a repository
+ */
+ #include <stdio.h>
+ 
+ int main (void){
+ 
+ printf("Hello World");
+ printf("I am majoring in Integrated Science");// line to represent my major
+ return 0;
+ }
